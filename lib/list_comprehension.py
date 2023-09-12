658e4df6a17397a52e20d30a644871dc97e33187
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-09-12 15:22:42 +0300

    Methods updated

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [i for i in num_list if (i%2 == 0)]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [word + "!" for word in sentence_list ]
+    return new_list
+
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
